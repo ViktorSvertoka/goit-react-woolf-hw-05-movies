2be2aaa16c3e2546e1d02da62c15ab5a27f4935b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-02-19 22:23:46 +0200

    ✅ Feat(JS & JSON) Added API services & npm package

---
+++ src/services/tmbdApi.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+const API_KEY = '992758a4802a699e8df27d4d6efc34fb';
+
+export const fetchTrending = async () => {
+  const response = await axios.get(`trending/movie/day?api_key=${API_KEY}`);
+  return response.data.results;
+};
+
+export const fetchSearchByKeyword = async keyword => {
+  const response = await axios.get(
+    `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${keyword}`
+  );
+  return response.data.results;
+};
+
+export const fetchMovieDetails = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}?api_key=${API_KEY}&language=en-US`
+  );
+  return response.data;
+};
+
+export const fetchActors = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
+  );
+  return response.data.cast;
+};
+
+export const fetchReviews = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return response.data.results;
+};
