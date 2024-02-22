bcf5368d2db4690c2c31bbc467b6bde976b12c36
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-02-22 22:42:18 +0200

    ✅ Fix(JSX) Change link

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from 'components/Layout/Layout';
 
 const Cast = lazy(() => import('components/Cast/Cast'));
-const Reviews = lazy(() => import('components/Reviews/Reviews.styled'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
 const Home = lazy(() => import('pages/Home/Home'));
 const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
 const Movies = lazy(() => import('pages/Movies/Movies'));
