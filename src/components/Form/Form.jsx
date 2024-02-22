a52d70c61e6a258a600631aeee999ad1875e8fe6
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-02-22 23:12:31 +0200

    ✅ Feat(JSX) Added styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { SearchForm, Input, Button } from './Form.styled';
+import { SearchForm } from './Form.styled';
 
 const Form = ({ searchMovies }) => {
   const [query, setQuery] = useState('');
@@ -15,14 +15,21 @@ const Form = ({ searchMovies }) => {
 
   return (
     <SearchForm onSubmit={handleSubmit}>
-      <Input
+      <input
+        className="mr-2 min-w-0 rounded-md border-2 border-[#64748b] bg-[#1da1f2]/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
         type="text"
         name="query"
         autoFocus
         value={query}
         onChange={handleInputChange}
+        placeholder="Enter Movie"
       />
-      <Button type="submit">Search</Button>
+      <button
+        className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+        type="submit"
+      >
+        Search
+      </button>
     </SearchForm>
   );
 };
