11d43f0aee7c9af77afffc028eab05205d52c4e4
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-02-25 13:39:16 +0200

    ✅ Feat(JSX) Refactoring code

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Loader from 'components/Loader/Loader';
 import EditorList from 'components/MovieList/MovieList';
 import Form from 'components/Form/Form';
@@ -8,26 +9,36 @@ const Movies = () => {
   const [searchFilms, setSearchFilms] = useState([]);
   const [loading, setLoading] = useState(false);
   const [noMoviesText, setNoMoviesText] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const movieName = searchParams.get('query') || '';
 
-  const searchMovies = async queryMovie => {
-    try {
-      setLoading(true);
-      const searchResults = await fetchSearchByKeyword(queryMovie);
-      setSearchFilms(searchResults);
-      setNoMoviesText(searchResults.length === 0);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
+  const updateQueryString = query => {
+    const nextParams = query !== '' && { query };
+    setSearchParams(nextParams);
   };
+
+  useEffect(() => {
+    const search = async () => {
+      try {
+        setLoading(true);
+        const movies = await fetchSearchByKeyword(movieName);
+        setSearchFilms(movies);
+        setNoMoviesText(movies.length === 0);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    search();
+  }, [movieName]);
 
   return (
     <main>
-      <Form searchMovies={searchMovies} />
+      <Form searchMovies={updateQueryString} />
       {loading && <Loader />}
       {noMoviesText && (
-        <p>There are no movies with this request. Please, try again</p>
+        <p>There are no movies with this request. Please, try again...</p>
       )}
       {searchFilms.length > 0 && <EditorList films={searchFilms} />}
     </main>
